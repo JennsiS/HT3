53bcc4c0f6e33e085b4e9ea45366a2a8db9b0969
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-02-08 22:47:03 -0600

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -1,33 +1,33 @@
-
-
 /**
  *
  * @author Andrea Paniagua, Jennifer  Sandoval
  * Carne: 18733, 18962
  * QuickSort.java
+ * Referencia: https://www.geeksforgeeks.org/quick-sort/
  * <p>Esta es una clase con el metodo de ordenamiento de numeros de tipo Quick </p>
  */
 public class QuickSort {
-    public int partition(int arr[], int low, int high) {
-        int pivot = arr[high];
+    int partition(Comparable arr[], int low, int high) { //Se usa comparable como indica hoja
+        Comparable pivot = arr[high]; //siempre inicia con un pivot en este caso , comparable
         int i = (low-1);
-        for (int j=low; j<high; j++){
-            if (arr[j] <= pivot){
+        for (int j=low; j<high; j++){ //para que funcione
+            if (arr[j].compareTo(pivot) < 0){ //compara arr con pivot
                 i++;
-                int temp = arr[i];
+                Comparable temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-        int temp = arr[i+1];
+        Comparable temp = arr[i+1]; //suma al comparable
         arr[i+1] = arr[high];
         arr[high] = temp;
 
         return i+1;
     }
 
-   public void sort(int arr[], int low, int high){
+    void sort(Comparable arr[], int low, int high){
         if (low < high){
+            //low menor que high
             int pi = partition(arr, low, high);
             sort(arr, low, pi-1);
             sort(arr, pi+1, high);
