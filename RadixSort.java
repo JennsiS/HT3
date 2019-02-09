6afefddf174147120c1126b4129f1f4664f0f592
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-02-08 23:36:39 -0600

    Add files via upload

--- RadixSort.java
+++ RadixSort.java
@@ -9,62 +9,19 @@ import java.util.Arrays;
  * <p>Esta clase tiene un metodo de ordenamiento de numeros de tipo Radix </p>
  */
 public class RadixSort {
+    void radixsort(Comparable arr[], int n) {
+        // Find the maximum number to know number of digits
+        Comparable m = getMax(arr, n);
 
-    /**
-     *
-     * @param arr
-     * @param n
-     * @return
-     */
-    public int getMax (int arr[], int n){
-        int mx = arr[0];
-        for (int i = 1; i < n; i++){
-            if (arr[i] > mx){
+        // No se cuenta poco a poco
+    }
+
+    Comparable getMax(Comparable arr[], int n) {
+        Comparable mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i].compareTo(mx) > 0)
                 mx = arr[i];
-            }
-        }
         return mx;
-    }
 
-    /**
-     *
-     * @param arr
-     * @param n
-     * @param exp
-     */
-    public void countSort(int arr[], int n, int exp) {
-        int output[] = new int[n];
-        int i;
-        int count[] = new int[10];
-        Arrays.fill(count, 0);
-
-        for (i = 0; i < n; i++) {
-            count[arr[i] / exp % 10]++;
-        }
-
-        for (i = 1; i < 10; i++) {
-            count[i] += count[i - 1];
-        }
-
-        for (i = n - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
-        }
-
-        for (i = 0; i < n; i++) {
-            arr[i] = output[i];
-        }
-    }
-
-    /**
-     *
-     * @param arr
-     * @param n
-     */
-    public void radixSort(int arr[], int n){
-        int m = this.getMax(arr, n);
-
-        for (int exp = 1; m/exp > 0; exp *= 10)
-            countSort(arr, n, exp);
     }
 }
